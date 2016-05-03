158c88b34e8d9c294bfb5918f69eac32f5cc0cc6
Alejandro <one.ugarch@gmail.com>
2016-05-03 23:05:26 +0200

    csahrp/shared hash set: Add initial implementation of SharedHashSet

---
+++ csharp/SharedHashSet/CheckExclusiveElements.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace SharedHashSet
+{
+    class CheckExclusiveElements
+    {
+        MyHashSet<int> set;
+        int min, max, repetitions;
+
+        /// <summary>
+        /// Inclusive min, exclusive max
+        /// </summary>
+        /// <param name="set"></param>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="repetitions"></param>
+        public CheckExclusiveElements(MyHashSet<int> set, int min, int max, int repetitions)
+        {
+            this.set = set;
+            this.min = min;
+            this.max = max;
+            this.repetitions = repetitions;
+        }
+
+        public void Run()
+        {
+            bool []state = new bool[max-min];
+            for (int i = 0; i < state.Length; i++)
+            {
+                state[i] = false;
+            }
+
+            Random rnd = new Random(System.DateTime.Now.Millisecond);
+            int element, index;
+            bool result, expected;
+            for (int i = 0; i < repetitions; i++)
+            {
+                index = rnd.Next(state.Length);
+                element = index + min;
+                
+                // Assert current state
+                expected = state[index];
+                result = set.Contains(element);
+                Debug.Assert(expected == result, "INCONSISTENCY!!!");
+
+                if (state[index])
+                {
+                    // The element is already in there
+
+                    // Check to see that adding it again fails
+                    expected = false;
+                    result = set.Add(element);
+                    Debug.Assert(expected == result, "INCONSISTENCY!!!");
+
+                    // Remove the element
+                    expected = true;
+                    result = set.Remove(element);
+                    Debug.Assert(expected == result, "INCONSISTENCY!!!");
+                    state[index] = false;
+                }
+                else
+                {
+                    // Check to see that removing it again fails
+                    expected = false;
+                    result = set.Remove(element);
+                    Debug.Assert(expected == result, "INCONSISTENCY!!!");
+
+                    // Check to see that adding it again works
+                    expected = true;
+                    result = set.Add(element);
+                    Debug.Assert(expected == result, "INCONSISTENCY!!!");
+                    state[index] = true;
+                }
+
+                // Assert current state
+                expected = state[index];
+                result = set.Contains(element);
+                Debug.Assert(expected == result, "INCONSISTENCY!!!");
+            }
+        }
+    }
+}
---
+++ csharp/SharedHashSet/ElementState.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SharedHashSet
+{
+    // Immutable object containing wether the current node is deleted and what his child is
+    class ElementState<T>
+    {
+        private readonly bool isDeleted;
+        private readonly Element<T> next;
+
+        public ElementState(bool isDeleted, Element<T> next)
+        {
+            this.isDeleted = isDeleted;
+            this.next = next;
+        }
+
+        public bool IsDeleted
+        {
+            get { return isDeleted; }
+        }
+
+        public Element<T> Next
+        {
+            get { return next; }
+        }
+    }
+}
