18cde71cc37cc58b18818ad94ea6712004d0b5e6
Alejandro <one.ugarch@gmail.com>
2016-05-23 23:24:27 +0200

    Fix Similarity Matrix

--- java/LocalitySensitiveHashing/src/com/IIgarcialII/lsh/SimilarityMatrixCreator.java
+++ java/LocalitySensitiveHashing/src/com/IIgarcialII/lsh/SimilarityMatrixCreator.java
@@ -1,48 +1,67 @@
 package com.IIgarcialII.lsh;
 
+/**
+ * @author Alejandro Garcia
+ */
 public class SimilarityMatrixCreator {
-	private final static int NUMBER_OF_HASH_FUNCTIONS = 600;
-	
-	public int[][] create(final String[] docs, final boolean[][] matrix) {
-		final int[][] result = new int[NUMBER_OF_HASH_FUNCTIONS][docs.length];
-		// int result to in max value
-		for(int row = 0; row < result.length; ++row) {
-			for(int col = 0; col < result[0].length; ++col) {
-				result[row][col] = Integer.MAX_VALUE;
-			}
-		}
-		
-		// number of buckets == the total number of shingles
-		final int numberOfBuckets = matrix.length;
-		int[] hashFunctionsCurrentValue = null;
-		// rows == shingles
-		for(int row = 0; row < matrix.length; ++row) {
-			// compute hash functions
-			// We make the row param non zero index
-			hashFunctionsCurrentValue = calculateHashFunctions(row + 1, numberOfBuckets);
-			// matrix cols == docs		
-			for(int col = 0; col < docs.length; ++col) {
-				if(matrix[row][col]) {
-					for(int resultRow = 0; resultRow < result.length; ++resultRow) {
-						if(result[resultRow][col] > hashFunctionsCurrentValue[resultRow]) {
-							result[resultRow][col] = hashFunctionsCurrentValue[resultRow];
-						}
-					}
-				}
-			}
-		}		
-		return result;
-	}
-	
-	// Generate NUMBER_OF_HASH_FUNCTIONS
-	private int[] calculateHashFunctions(final int rowNumber, final int numberOfBuckets) {
-		final int[] result = new int[NUMBER_OF_HASH_FUNCTIONS];
-		int h = rowNumber;
-		result[0] = (h ^ 1) % numberOfBuckets;
-		for(int i = 1; i < NUMBER_OF_HASH_FUNCTIONS; ++i) {
-			result[i] = (h ^ (i + 1)) % numberOfBuckets;
-		}
-		return result;
-	}
+
+        private final int numberOfHashFunctions;
+
+        public SimilarityMatrixCreator(int numberOfHashFunctions) {
+                this.numberOfHashFunctions = numberOfHashFunctions;
+        }
+
+        /**
+         * Generates a similarity matrix
+         *
+         * @param matrix
+         * @return
+         */
+        public int[][] create(final boolean[][] matrix) {
+                final int[][] result = new int[matrix.length][numberOfHashFunctions];
+                // int result to in max value, row docs and cols hash functions
+                for (int row = 0; row < result.length; ++row) {
+                        for (int col = 0; col < numberOfHashFunctions; ++col) {
+                                result[row][col] = Integer.MAX_VALUE;
+                        }
+                }
+
+                // number of buckets == the total number of shingles
+                final int numberOfBuckets = matrix[0].length;
+                int[] hashFunctionsCurrentValue = null;
+
+                // rows == docs
+                for (int row = 0; row < matrix.length; ++row) {
+                        // col = shingles in boolean matrix
+                        for (int col = 0; col < matrix[0].length; ++col) {
+                                hashFunctionsCurrentValue = calculateHashFunctions(col + 1, numberOfBuckets);
+                                if (matrix[row][col]) {
+                                        for (int resultRow = 0; resultRow < numberOfHashFunctions; ++resultRow) {
+                                                if (result[row][resultRow] > hashFunctionsCurrentValue[resultRow]) {
+                                                        result[row][resultRow] = hashFunctionsCurrentValue[resultRow];
+                                                }
+                                        }
+                                }
+                        }
+                }
+                return result;
+        }
+
+        /**
+         * Generate NUMBER_OF_HASH_FUNCTIONS
+         *
+         * @param colNumber
+         * @param numberOfBuckets
+         * @return
+         */
+        private int[] calculateHashFunctions(final int colNumber, final int numberOfBuckets) {
+                final int[] result = new int[numberOfHashFunctions];
+                int h = colNumber;
+                result[0] = (h ^ 1) % numberOfBuckets;
+                for (int i = 1; i < numberOfHashFunctions; ++i) {
+                        result[i] = (h ^ (i + 1)) % numberOfBuckets;
+                }
+                return result;
+        }
 
 }
