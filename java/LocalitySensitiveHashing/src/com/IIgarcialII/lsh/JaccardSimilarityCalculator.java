aedf09f3abf7d53fc2d5be462dd2c0e3368a0412
Alejandro <one.ugarch@gmail.com>
2016-05-23 22:01:51 +0200

    Fix Shingler and Jaccard similarity calculator

--- java/LocalitySensitiveHashing/src/com/IIgarcialII/lsh/JaccardSimilarityCalculator.java
+++ java/LocalitySensitiveHashing/src/com/IIgarcialII/lsh/JaccardSimilarityCalculator.java
@@ -1,47 +1,50 @@
 /**
- * 
+ *
  */
 package com.IIgarcialII.lsh;
 
 /**
  * @author Alejandro
- *
  */
 public class JaccardSimilarityCalculator {
-	
-	public double[][] calculate(final boolean[][] matrix) {
-		double[][] result = new double[matrix[0].length][matrix[0].length];
-		boolean[] docOne = null, docTwo = null;
-		for(int i = 0; i < result.length; ++i) {
-			for(int j = i + 1; j < result.length; ++j) {
-				docOne = getShinglesArray(matrix, i);
-				docTwo = getShinglesArray(matrix, j);
-				result[i][j] = computeJaccard(docOne, docTwo);
-				result[j][i] = result[i][j];
-			}
-		}
-		return result;
-	}
-	
-	private boolean[] getShinglesArray(final boolean[][] matrix, int docIndex) {
-		final boolean[] result = new boolean[matrix.length];
-		for(int i = 0; i < matrix.length; ++i) {
-			result[i] = matrix[i][docIndex];
-		}
-		return result;
-	}
-	
-	private double computeJaccard(boolean[] docOne, boolean[] docTwo) {
-		double intersect = 0;
-		double union = 0;
-		for(int i = 0; i < docOne.length; ++i) {
-			if(docOne[i] == docTwo[i] && (docOne[i] != false || docTwo[i] != false)) {
-				intersect++;
-				union++;
-			}else if(docOne[i] != false || docTwo[i] != false) {
-				union++;
-			}
-		}
-		return union != 0 ? intersect/union : 0;
-	}
+
+        /**
+         * @param matrix
+         * @return
+         */
+        public double[][] calculate(final boolean[][] matrix) {
+                // documents number are in rows
+                double[][] result = new double[matrix.length][matrix.length];
+                // shingles boolean array
+                boolean[] docOne = null, docTwo = null;
+                for (int i = 0; i < result.length; ++i) {
+                        docOne = matrix[i];
+                        for (int j = i + 1; j < result.length; ++j) {
+                                docTwo = matrix[j];
+                                result[i][j] = computeJaccard(docOne, docTwo);
+                                // symmetric matrix
+                                result[j][i] = result[i][j];
+                        }
+                }
+                return result;
+        }
+
+        /**
+         * @param docOne
+         * @param docTwo
+         * @return
+         */
+        private double computeJaccard(boolean[] docOne, boolean[] docTwo) {
+                double intersect = 0;
+                double union = 0;
+                for (int i = 0; i < docOne.length; ++i) {
+                        if (docOne[i] && docTwo[i]) {
+                                intersect++;
+                                union++;
+                        } else if (docOne[i] || docTwo[i]) {
+                                union++;
+                        }
+                }
+                return union != 0 ? intersect / union : 0;
+        }
 }
